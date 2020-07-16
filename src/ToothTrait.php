8791756a9daff98af4f888f56d9ed9817f4ad211
Puleeno Nguyen <puleeno@gmail.com>
2020-07-16 19:20:09 +0700

    Init project structure

---
+++ src/ToothTrait.php
@@ -0,0 +1,10 @@
+<?php
+namespace Puleeno\Rake\WordPress;
+
+use Ramphor\Rake\Resource;
+
+trait ToothTrait {
+    public function downloadResource(Resource $resouce): Resource {
+        return $resouce;
+    }
+}
