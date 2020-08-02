3ab6ad307e2d984e6f6a5d3ffbd8abc094da9f4f
Puleeno Nguyen <puleeno@gmail.com>
2020-08-02 16:07:47 +0700

    Added new logs to WordPress processor

--- src/Content/WordPressProcessor.php
+++ src/Content/WordPressProcessor.php
@@ -16,66 +16,78 @@ abstract class WordPressProcessor extends Processor
     {
         $document = new Dom();
         $document->load($this->feedItem->content);
-        $gallaryImages = $this->feedItem->galleryImages;
+        $gallaryImages  = $this->feedItem->galleryImages;
+        $totalGalleries = count($gallaryImages);
 
-        foreach ($document->find('div.gallery[id^="gallery"]') as $gallery) {
-            $images     = [];
-            $attributes = [];
-            $classes    = $gallery->getAttribute('class');
+        Logger::debug(sprintf('The processor founded %d WordPress gallery', $totalGalleries));
+        if ($totalGalleries > 0) {
+            foreach ($document->find('div.gallery[id^="gallery"]') as $gallery) {
+                $images     = [];
+                $attributes = [];
+                $classes    = $gallery->getAttribute('class');
+                $domImages  = $gallery->find('img');
 
-            foreach ($gallery->find('img') as $image) {
-                $parent = $image->getParent();
-                if ($parent->tag->name() === "a") {
-                    $imageUrl = $parent->getAttribute('href');
-                    if (!preg_match('/\.\w{2,}$/', $imageUrl)) {
+                Logger::debug('Found %d from the content', count($domImages));
+                foreach ($domImages as $image) {
+                    $parent = $image->getParent();
+                    if ($parent->tag->name() === "a") {
+                        $imageUrl = $parent->getAttribute('href');
+                        if (!preg_match('/\.\w{2,}$/', $imageUrl)) {
+                            $imageUrl = $this->convertImageUrl($image->getAttribute('src'));
+                        } elseif (empty($attributes['link'])) {
+                            $attributes['link'] = 'file';
+                        }
+                    } else {
                         $imageUrl = $this->convertImageUrl($image->getAttribute('src'));
-                    } elseif (empty($attributes['link'])) {
-                        $attributes['link'] = 'file';
                     }
-                } else {
-                    $imageUrl = $this->convertImageUrl($image->getAttribute('src'));
+
+                    // Check image is not deleted else remove it in gallery
+                    if (!$this->checkImageIsFound($imageUrl)) {
+                        Logger::warning(sprintf('The image has URL %s is not found. It\'s removed in content', $imageUrl), [
+                            'processor' => get_class($this),
+                            'tooth' => $this->tooth->getId()
+                        ]);
+                        continue;
+                    }
+
+                    $images[]        = $imageUrl;
+                    $gallaryImages[] = $imageUrl;
                 }
 
-                // Check image is not deleted else remove it in gallery
-                if (!$this->checkImageIsFound($imageUrl)) {
-                    Logger::warning(sprintf('The image has URL %s is not found', $imageUrl), [
-                        'processor' => get_class($this),
-                        'tooth' => $this->tooth->getId()
-                    ]);
-                    continue;
+
+                if (preg_match('/gallery\-columns\-(\d{1,})/', $classes, $matches) && $matches[1] != 3) {
+                    $attributes['columns'] = $matches[1];
+                }
+                if (preg_match('/gallery\-size\-(\w{1,})/', $classes, $matches)) {
+                    $attributes['size'] = $matches[1];
                 }
 
-                $images[]        = $imageUrl;
-                $gallaryImages[] = $imageUrl;
-            }
+                $attributes      = apply_filters('rake_wordpress_gallery_attributes', $attributes);
+                $new_shortcode   = apply_filters('rake_wordpress_gallery_shortcode', 'gallery');
+                $attributes_text = '';
 
-
-            if (preg_match('/gallery\-columns\-(\d{1,})/', $classes, $matches) && $matches[1] != 3) {
-                $attributes['columns'] = $matches[1];
-            }
-            if (preg_match('/gallery\-size\-(\w{1,})/', $classes, $matches)) {
-                $attributes['size'] = $matches[1];
-            }
-
-            $attributes      = apply_filters('rake_wordpress_gallery_attributes', $attributes);
-            $new_shortcode   = apply_filters('rake_wordpress_gallery_shortcode', 'gallery');
-            $attributes_text = '';
-
-            if (!empty($attributes)) {
-                foreach ($attributes as $attribute => $value) {
-                    $attributes_text .= sprintf(' %s="%s"', $attribute, $value);
+                if (!empty($attributes)) {
+                    foreach ($attributes as $attribute => $value) {
+                        $attributes_text .= sprintf(' %s="%s"', $attribute, $value);
+                    }
                 }
+                $gallery_shortcode = new TextNode(sprintf(
+                    '[%s%s ids="%s"]',
+                    $new_shortcode,
+                    $attributes_text,
+                    implode(', ', $images),
+                ));
+
+                Loggerr::debug(sprintf(
+                    'Convert HTML gallery %s to WordPress gallery %s',
+                    $gallery->innertHtml,
+                    $gallery_shortcode->innerHtml
+                ));
+                $gallery->getParent()->replaceChild($gallery->id(), $gallery_shortcode);
             }
-            $gallery_shortcode = new TextNode(sprintf(
-                '[%s%s ids="%s"]',
-                $new_shortcode,
-                $attributes_text,
-                implode(', ', $images),
-            ));
-            $gallery->getParent()->replaceChild($gallery->id(), $gallery_shortcode);
+            $this->feedItem->setProperty('galleryImages', $gallaryImages);
+            $this->feedItem->setProperty('content', $document->innerHtml);
         }
-        $this->feedItem->setProperty('galleryImages', $gallaryImages);
-        $this->feedItem->setProperty('content', $document->innerHtml);
     }
 
     public function convertTableOfContent()
@@ -100,10 +112,15 @@ abstract class WordPressProcessor extends Processor
      */
     public function convertImageUrl($imageUrl)
     {
-        return preg_replace(
+        $convertedUrl = preg_replace(
             '/\-\d{1,}x\d{1,}(\.\w{2,})$/',
             '$1',
             $imageUrl
         );
+
+        if ($imageUrl !== $convertedUrl) {
+            Logger::debug('The image %s is converted to %s', $imageUrl, $convertedUrl);
+        }
+        return $convertedUrl;
     }
 }
