34eee5746b6c39d69d6bbab529a00503410176da
Puleeno Nguyen <puleeno@gmail.com>
2020-08-02 19:25:26 +0700

    Import simple product

--- src/Traits/WordPressProcessor.php
+++ src/Traits/WordPressProcessor.php
@@ -3,4 +3,28 @@ namespace Puleeno\Rake\WordPress\Traits;
 
 trait WordPressProcessor
 {
+    public function importPost($postContent = null)
+    {
+    }
+
+    public function importPage($pageContent = null)
+    {
+    }
+
+    public function checkIsExists($postTitle, $originalId)
+    {
+    }
+
+    public function convertPostStatus($postStatus)
+    {
+        if (!in_array($postStatus, ['publish', 'draft', 'pending'])) {
+            $postStatus = apply_filters(
+                'rake_default_post_status',
+                'publish',
+                $postStatus,
+                $this->feedItem
+            );
+        }
+        return $postStatus;
+    }
 }
